562bc181838e1d61dc6d9d7dd7e2c3d4a69d781b
Bùi Hữu Đông <buihuudong19@gmail.com>
2024-09-04 18:35:25 +0700

    Create UpdateTranById.go

---
+++ Chaincodes in Hyperledger Fabric/UpdateTranById.go
@@ -0,0 +1,25 @@
+func (s *EWalletContract) UpdateTranById(
+    ctx contractapi.TransactionContextInterface, 
+    assetID string,  
+    createDate time.Time,
+    tranHash string) error {
+    exists, err := s.AssetExists(ctx, id)
+    if err != nil { return err }
+    if !exists {
+        return fmt.Errorf("the asset %s does not exist", id)
+    }
+    asset := &Asset{
+        Id:             assetID,
+        CreateDate:     createDate,
+        TranHash:       tranHash,
+    }
+    assetJSON, err := json.Marshal(asset)
+    if err != nil { return err}
+    err = ctx.GetStub().PutState(id, assetJSON)
+    if err != nil { return err }
+    // Registering the event when update ledger
+    eventPayload := fmt.Sprintf("Asset with ID %s updated", assetID)
+    err = ctx.GetStub().SetEvent("UpdateTransEvent", []byte(eventPayload))
+    if err != nil { return fmt.Errorf("Failed to set event: %v", err)}
+    return nil
+}
