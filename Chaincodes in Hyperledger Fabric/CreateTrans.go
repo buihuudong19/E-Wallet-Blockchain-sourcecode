79c33299f301fdc5e4a578c8d17fb1495458a5c7
Bùi Hữu Đông <buihuudong19@gmail.com>
2024-09-04 18:32:48 +0700

    Create CreateTrans.go

---
+++ Chaincodes in Hyperledger Fabric/CreateTrans.go
@@ -0,0 +1,27 @@
+func (t *EwalletContract) CreateTrans(
+    ctx contractapi.TransactionContextInterface, 
+    assetID  string, createDate time.Time, tranHash string) error {
+    exists, err := t.AssetExists(ctx, assetID)
+    if err != nil {
+        return fmt.Errorf("Failed to get Asset: %v", err)
+    }
+    if exists {
+        return fmt.Errorf("Asset already exists: %s", assetID)
+    }
+    asset := &Asset{
+        Id:             assetID,
+        CreateDate:     createDate,
+        TranHash:       tranHash,
+    }
+    assetBytes, err := json.Marshal(asset)
+    if err != nil {
+        return err
+    }
+    err = ctx.GetStub().PutState(assetID, assetBytes)
+    if err != nil { return err }
+    // Registering the event
+    eventPayload := fmt.Sprintf("Asset with ID %s created", assetID)
+    err = ctx.GetStub().SetEvent("CreateTransEvent", []byte(eventPayload))
+    if err != nil { return fmt.Errorf("Failed to set event: %v", err)}
+    return nil
+}
